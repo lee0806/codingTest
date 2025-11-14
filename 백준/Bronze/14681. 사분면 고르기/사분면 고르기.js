e0b0f9e9d94a9cd65c9149ed6f30c502d97160a1
sehyun lee <mh080411@kyonggi.ac.kr>
2025-11-15 00:56:07 +0900

    [Bronze V] Title: 사분면 고르기, Time: 88 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8')
+  .toString()
+  .trim()
+  .split(/\s+/);
+
+const X = Number(input[0]);
+const Y = Number(input[1]);
+
+if (X > 0 && Y > 0) console.log(1);
+else if (X < 0 && Y > 0) console.log(2);
+else if (X < 0 && Y < 0) console.log(3);
+else if (X > 0 && Y < 0) console.log(4);
